4a108566b55dd7d205d047181765a06023236784
choigone <gowoonvv@gmail.com>
2019-12-03 15:39:09 -0500

    add gpio cpp code

---
+++ GPIO_CPP/cmake-build-debug/CMakeFiles/GPIO_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GPIO_CPP.dir/main.cpp.o"
+  "GPIO_CPP"
+  "GPIO_CPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GPIO_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
